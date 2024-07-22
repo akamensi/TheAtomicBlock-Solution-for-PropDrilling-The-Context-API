e25e56b75a883561f27c66eb8217bc9d02c8477e
akamensi <akez.muhammed@gmail.com>
2024-07-22 19:13:43 +0200

    Advanced Pattern: Acustom Provider and Hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
+import { PostPorvider, PostContext } from "./PostProvider";
 
 function createRandomPost() {
   return {
@@ -9,10 +10,10 @@ function createRandomPost() {
 }
 
 //1) create a new context
-const PostContext = createContext();
+//const PostContext = createContext();
 
 function App() {
-  const [posts, setPosts] = useState(() =>
+  /*   const [posts, setPosts] = useState(() =>
     Array.from({ length: 30 }, () => createRandomPost())
   );
   const [searchQuery, setSearchQuery] = useState("");
@@ -34,8 +35,9 @@ function App() {
 
   function handleClearPosts() {
     setPosts([]);
-  }
+  } */
 
+  const [isFakeDark, setIsFakeDark] = useState(false);
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
@@ -45,7 +47,7 @@ function App() {
   );
 
   return (
-    //2) Porvide value to child components
+    /*     //2) Porvide value to child components
     <PostContext.Provider
       value={{
         posts: searchedPosts,
@@ -54,21 +56,22 @@ function App() {
         searchQuery,
         setSearchQuery,
       }}
-    >
-      <section>
-        <button
-          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-          className="btn-fake-dark-mode"
-        >
-          {isFakeDark ? "☀️" : "🌙"}
-        </button>
-
+    > */
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+      <PostPorvider>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </section>
-    </PostContext.Provider>
+      </PostPorvider>
+    </section>
+    //</PostContext.Provider>
   );
 }
 
