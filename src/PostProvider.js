b7703409549687e093aa99136b8110cfb88cf914
akamensi <akez.muhammed@gmail.com>
2024-07-22 19:52:03 +0200

    Advanced Pattern: Acustom Provider and Hook

--- src/PostProvider.js
+++ src/PostProvider.js
@@ -50,6 +50,8 @@ function PostPorvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider");
   return context;
 }
 
