569604fd48c7f476de71d1c56887699c23ffeb44
akamensi <akez.muhammed@gmail.com>
2024-07-28 22:58:23 +0200

    Undrestand How react render components with Profiler tool

--- src/Test.js
+++ src/Test.js
@@ -1,26 +1,12 @@
-import { useState } from "react";
-
-function SlowComponent() {
-  // If this is too slow on your maching, reduce the `length`
-  const words = Array.from({ length: 100_000 }, () => "WORD");
-  return (
-    <ul>
-      {words.map((word, i) => (
-        <li key={i}>
-          {i}: {word}
-        </li>
-      ))}
-    </ul>
-  );
-}
+import Counter from "./components/Counter";
+import SlowComponent from "./components/SlowComponent";
 
 export default function Test() {
-  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Slow counter?!?</h1>
-      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      {children}
+    </div>
+  );
+}
+
+export default Counter;
